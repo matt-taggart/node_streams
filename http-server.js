8cc698931f57a2042d176ef30873d35bcbe65adc
Matthew Taggart <matthew.taggart@cardcash.com>
2017-05-18 22:58:20 -0400

    Added object stream && http server examples.

---
+++ http-server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer(function (req, res) {
+  if (req.method === 'POST') {
+    req.pipe(process.stdout);
+    req.once('end', function() {
+      res.end('ok\n');
+    })  
+  } else {
+    res.setHeader('content-type', 'text/plain');
+    fs.createReadStream('hello.txt').pipe(res);
+  }
+});
+
+server.listen(5000);
